2919efcdaa164f5e9b71681c73d60aa4383a7552
kareem <kareemesam52@yahoo.com>
2023-07-12 16:51:16 +0300

    add resources paths and update dimens

--- src/jvmMain/kotlin/ui/theme/Dimens.kt
+++ src/jvmMain/kotlin/ui/theme/Dimens.kt
@@ -1,11 +1,15 @@
 package ui.theme
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 val textSize128 = 128.sp
 val textSize80 = 80.sp
 val textSize24 = 24.sp
 val textSize16 = 16.sp
+
+val Space8 = 8.dp
+
 
 const val blur24 = 0.25f
 const val blur35 = 0.35f
