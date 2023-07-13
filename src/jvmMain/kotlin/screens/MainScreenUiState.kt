84b2e22dad0b45a61af62ce35424526857fc0921
youssef <yr925344git@gmail.com>
2023-07-13 23:59:59 +0300

    update main screen ui state

--- src/jvmMain/kotlin/screens/MainScreenUiState.kt
+++ src/jvmMain/kotlin/screens/MainScreenUiState.kt
@@ -1,26 +1,30 @@
 package screens
 
 data class HourlyUiState(
-    val values: List<DayHourIntervals>
+    val lat: Double = 0.0,
+    val long: Double = 0.0,
+    val values: List<DayHourIntervals> = emptyList()
 )
 
 data class DailyUiState(
-    val todayTemp: Int,
-    val todayMinTemp: Int,
-    val windSpeed: Double,
-    val precipitation: Int,
-    val days: List<DaysInterval>
+    val todayTemp: Int = 0,
+    val todayMinTemp: Int = 0,
+    val windSpeed: Double = 0.0,
+    val precipitation: Int = 0,
+    val lat: Double = 0.0,
+    val long: Double = 0.0,
+    val days: List<DaysInterval> = emptyList(),
 )
 
 data class DayHourIntervals(
-    val hour: String,
-    val weatherType: String,
-    val temp: Int,
+    val hour: String = "",
+    val weatherType: String = "",
+    val temp: Int = 0,
 )
 
 data class DaysInterval(
-    val day: String,
-    val weatherType: String,
-    val maxTemp: Int,
-    val minTemp: Int,
+    val day: String = "",
+    val weatherType: String = "",
+    val maxTemp: Int = 0,
+    val minTemp: Int = 0,
 )
