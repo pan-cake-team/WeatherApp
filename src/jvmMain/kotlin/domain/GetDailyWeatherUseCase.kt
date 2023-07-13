7bc043eea5ddf1d7e03f7663cfb2032280e8a1a4
kareem <kareemesam52@yahoo.com>
2023-07-13 23:40:36 +0300

    add DailyWeatherMapper and update UseCase

--- src/jvmMain/kotlin/domain/GetDailyWeatherUseCase.kt
+++ src/jvmMain/kotlin/domain/GetDailyWeatherUseCase.kt
@@ -2,12 +2,38 @@ package domain
 
 import data.domain.entity.DailyWeather
 import data.repository.WeatherRepository
+import screens.DailyUiState
+import screens.DaysInterval
 
 
 class GetDailyWeatherUseCase(
     private val dailyWeatherResponse: WeatherRepository
 ) {
-    suspend operator fun invoke(lat: Double, lon: Double): List<DailyWeather> {
-        return dailyWeatherResponse.getDailyWeather(lat, lon)
+    suspend operator fun invoke(lat: Double, lon: Double): DailyUiState {
+        return dailyWeatherResponse.getDailyWeather(lat, lon).toDailyUiState()
+    }
+}
+
+private fun List<DailyWeather>.toDailyUiState(): DailyUiState {
+    return this.let { dailyWeathers ->
+        DailyUiState(
+            todayTemp = dailyWeathers[0].temperature.toInt(),
+            todayMinTemp = dailyWeathers[0].temperatureMin.toInt(),
+            windSpeed = dailyWeathers[0].windSpeed,
+            precipitation = dailyWeathers[0].windSpeed.toInt(),
+            days = dailyWeathers.drop(1).map { it.toDailyInterval() },
+        )
+    }
+
+}
+
+private fun DailyWeather.toDailyInterval(): DaysInterval {
+    return this.let {
+        DaysInterval(
+            day = date,
+            minTemp = temperatureMin.toInt(),
+            maxTemp = temperature.toInt(),
+            weatherType = weatherCode.toString()
+        )
     }
 }
