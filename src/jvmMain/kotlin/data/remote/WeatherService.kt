bcbd4fa4c4bce6fdb01c13d807ca23573d92f094
youssef <yr925344git@gmail.com>
2023-07-13 19:09:59 +0300

    add current location service

--- src/jvmMain/kotlin/data/remote/WeatherService.kt
+++ src/jvmMain/kotlin/data/remote/WeatherService.kt
@@ -1,8 +1,11 @@
 package data.remote
 
+import data.remote.dto.Location
 import data.remote.dto.WeatherResponse
 
 interface WeatherService {
     suspend fun getDailyWeather(): WeatherResponse
     suspend fun getHourWeather(): WeatherResponse
+
+    suspend fun getLocation(): Location
 }
--- src/jvmMain/kotlin/data/remote/WeatherServiceImp.kt
+++ src/jvmMain/kotlin/data/remote/WeatherServiceImp.kt
@@ -1,5 +1,7 @@
 package data.remote
 
+import com.google.gson.Gson
+import data.remote.dto.Location
 import data.remote.dto.WeatherResponse
 import di.weatherModule
 import io.ktor.client.*
@@ -9,11 +11,13 @@ import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.client.request.get
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 import kotlinx.serialization.json.Json
 import org.koin.core.Koin
-import org.koin.core.context.GlobalContext
 import org.koin.core.context.startKoin
+import java.net.URL
 
 class WeatherServiceImp(
     private val client: HttpClient,
@@ -51,6 +55,16 @@ class WeatherServiceImp(
             url(url)
         }.body<WeatherResponse>()
         return response
+    }
+
+    override suspend fun getLocation(): Location {
+        return withContext(Dispatchers.IO) {
+            val url = URL("https://ipinfo.io/json")
+            val json = url.readText()
+            val data = Gson().fromJson(json, Location::class.java)
+            val location = data.loc
+            Location(loc = location)
+        }
     }
 }
 
