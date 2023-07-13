e0f458494612322f138b7817cefbfda136c60cdf
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-07-13 18:23:53 +0300

    create repository

--- src/jvmMain/kotlin/data/repo/WeatherRepo.kt
+++ src/jvmMain/kotlin/data/repo/WeatherRepo.kt
@@ -1,5 +1,11 @@
 package data.repo
 
+import data.remote.dto.WeatherResponse
+
 interface WeatherRepo {
+
+    suspend fun getDailyWeather(): List<WeatherResponse>
+    suspend fun getHourlyWeather(): List<WeatherResponse>
+
 
 }
--- src/jvmMain/kotlin/data/repo/WeatherRepoImpl.kt
+++ src/jvmMain/kotlin/data/repo/WeatherRepoImpl.kt
@@ -1,9 +1,18 @@
 package data.repo
 
 import data.remote.WeatherService
+import data.remote.dto.WeatherResponse
 
 class WeatherRepoImpl(
     weatherService: WeatherService,
 ) : WeatherRepo{
+
+    override suspend fun getDailyWeather(): List<WeatherResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getHourlyWeather(): List<WeatherResponse> {
+        TODO("Not yet implemented")
+    }
 
 }
