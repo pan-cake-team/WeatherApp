40ffbd61b1fec7e09cdb1088a5a14f2168972891
youssef <yr925344git@gmail.com>
2023-07-14 00:37:06 +0300

    update GetCurrentLocationUseCase

--- src/jvmMain/kotlin/domain/GetCurrentLocationUseCase.kt
+++ src/jvmMain/kotlin/domain/GetCurrentLocationUseCase.kt
@@ -1,12 +1,17 @@
 package domain
 
-import data.remote.dto.Location
 import data.repository.WeatherRepository
+import domain.model.LatLongLocation
 
 class GetCurrentLocationUseCase(
     private val location: WeatherRepository
 ) {
-    suspend operator fun invoke(): Location {
-        return location.getCurrentLocation()
+    suspend operator fun invoke(): LatLongLocation {
+        val loc = location.getCurrentLocation()
+            .loc.split(",")
+            .map { it.toDouble() }
+
+        val (lat, long) = loc
+        return LatLongLocation(lat, long)
     }
 }
