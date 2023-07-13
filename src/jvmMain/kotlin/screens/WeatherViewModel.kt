6966ea3176c009039b9095fb3aec5ce44cfbb379
youssef <yr925344git@gmail.com>
2023-07-13 18:43:18 +0300

    implement ViewModel

--- src/jvmMain/kotlin/screens/WeatherViewModel.kt
+++ src/jvmMain/kotlin/screens/WeatherViewModel.kt
@@ -1,7 +1,31 @@
 package screens
 
+import domain.GetWeatherUseCase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class WeatherViewModel {
+class WeatherViewModel(
+    private val getWeatherUseCase: GetWeatherUseCase,
+) {
+
+    private val _weatherUiState = MutableStateFlow(WeatherUiState())
+    val weatherUiState = _weatherUiState.asStateFlow()
+
+    private val viewModuleScope = CoroutineScope(Dispatchers.IO)
+
+    init {
+        getWeatherData()
+    }
+
+    private fun getWeatherData() {
+        viewModuleScope.launch {
+            getWeatherUseCase.GetWeatherData()
+            viewModuleScope.cancel()
+        }
+    }
 
 }
