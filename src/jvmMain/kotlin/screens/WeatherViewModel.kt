98db71150540f7937bf67cd848448d93a0d5977b
youssef <yr925344git@gmail.com>
2023-07-14 00:38:40 +0300

    update and get hourly data function

--- src/jvmMain/kotlin/screens/WeatherViewModel.kt
+++ src/jvmMain/kotlin/screens/WeatherViewModel.kt
@@ -8,12 +8,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class WeatherViewModel(
     private val getCurrentLocation: GetCurrentLocationUseCase,
     private val getHourlyWeather: GetHourlyWeatherUseCase,
-    private val getDailyWeatherUseCase: GetDailyWeatherUseCase,
+    private val getDailyWeather: GetDailyWeatherUseCase,
 //    private val getCityLocation: GetCityLocationUseCase,
     ) {
 
@@ -26,21 +27,36 @@ class WeatherViewModel(
     private val viewModuleScope = CoroutineScope(Dispatchers.IO)
 
     init {
+        getHourlyWeatherData()
         getDailyWeatherData()
     }
 
-    private fun getDailyWeatherData() {
+    private fun getHourlyWeatherData() {
         viewModuleScope.launch {
-            // use ui state
-            getDailyWeatherUseCase(22.9 ,11.2)
+            getCurrentLatLong()
+            val lat = _dailyUiState.value.lat
+            val long = _dailyUiState.value.long
+            getHourlyWeather(lat ,long)
             viewModuleScope.cancel()
         }
     }
 
-    private fun getCurrentLocation() {
+    private fun getDailyWeatherData() {
+        viewModuleScope.launch {
+            getCurrentLatLong()
+            val lat = _dailyUiState.value.lat
+            val long = _dailyUiState.value.long
+            getDailyWeather(lat ,long)
+            viewModuleScope.cancel()
+        }
+    }
+
+    private fun getCurrentLatLong() {
         viewModuleScope.launch {
             val currentLocation = getCurrentLocation()
-            // update ui state daily and hourly
+            val (lat,long) = currentLocation
+            _dailyUiState.update { it.copy(lat = lat, long = long) }
+            _hourlyUiState.update { it.copy(lat = lat, long = long) }
             viewModuleScope.cancel()
         }
     }
