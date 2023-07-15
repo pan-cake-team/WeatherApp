797aabd92330cb46996baa0528e66a817745aca3
YRRAAMM <yr925344git@gmail.com>
2023-07-15 18:25:03 +0300

    update get current location use case

--- src/jvmMain/kotlin/domain/GetCurrentLocationUseCase.kt
+++ src/jvmMain/kotlin/domain/GetCurrentLocationUseCase.kt
@@ -2,10 +2,13 @@ package domain
 
 import data.remote.dto.CurrentLocation
 import data.repository.WeatherRepository
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-class GetCurrentLocationUseCase(
-    private val location: WeatherRepository
-) {
+class GetCurrentLocationUseCase: KoinComponent{
+
+    private val location: WeatherRepository by inject()
+
     suspend operator fun invoke(): CurrentLocation {
         return location.getCurrentLocation()
     }
