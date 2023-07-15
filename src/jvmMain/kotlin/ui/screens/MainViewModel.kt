33e4af5ded33e540785a936c76085afcf1a15fae
YRRAAMM <yr925344git@gmail.com>
2023-07-15 17:46:42 +0300

    update main view model

--- src/jvmMain/kotlin/ui/screens/MainViewModel.kt
+++ src/jvmMain/kotlin/ui/screens/MainViewModel.kt
@@ -1,54 +1,78 @@
 package ui.screens
 
-import domain.GetCityLocationUseCase
-//import domain.GetDailyWeatherUseCase
-import domain.GetHourlyWeatherUseCase
+import domain.GetWeatherUseCase
+import domain.SearchCityUseCase
+import domain.model.DaysForCast
+import domain.model.HourlyWeather
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import ui.base.BaseViewModel
 
-class MainViewModel : KoinComponent ,BaseViewModel<HourlyUiState,DailyUiState>(HourlyUiState(), DailyUiState())  {
+class MainViewModel : KoinComponent ,BaseViewModel<MainUIState>(MainUIState())  {
 
-    private val getHourlyWeather: GetHourlyWeatherUseCase by inject()
-//    private val getDailyWeather: GetDailyWeatherUseCase by inject()
-    private val getCityLocation: GetCityLocationUseCase by inject()}
+    private val getDailyWeather: GetWeatherUseCase by inject()
+    private val searchCityUseCase: SearchCityUseCase by inject()
 
-//    init {
-//        getHourlyWeatherData()
-//        getDailyWeatherData()
-//    }
-//
-//    private fun getHourlyWeatherData() {
-//        viewModelScope.launch {
-//            val getHourlyWeather = getHourlyWeather()
-//            val getDailyWeather = getDailyWeather()
-//            updateHourlyState {
-//                it.copy(
-//                    isLoading = false,
-//                    date = getDailyWeather[0].day,
-//                    weatherType = getDailyWeather[0].weatherType,
-//                    values = getHourlyWeather
-//                )
-//            }
-//        }
-//    }
-//
-//    private fun getDailyWeatherData() {
-//        viewModelScope.launch {
-//            val getDailyWeather = getDailyWeather()
-//            updateDailyState {
-//                it.copy(
-//                    isLoading = false,
-//                    todayTemp = getDailyWeather[0].maxTemp,
-//                    todayMinTemp = getDailyWeather[0].minTemp,
-//                    windSpeed = getDailyWeather[0].windSpeed,
-//                    precipitation = getDailyWeather[0].windGust.toInt(),
-//                    days = getDailyWeather
-//                )
-//            }
-//        }
-//    }
-//
-//
-//}
+
+    init {
+        getHourlyWeatherData()
+        getDailyWeatherData()
+    }
+
+    private fun getHourlyWeatherData() {
+        viewModelScope.launch {
+            val getDailyWeather = getDailyWeather()
+            updateState {
+                it.copy(
+                    isLoading = false,
+                    date = getDailyWeather.data?.get(0)?.date!!,
+                    weatherType = getDailyWeather.data[0].weatherType!!,
+                    hours = getDailyWeather.data[0].days!!.map { hourlyWeather -> hourlyWeather.toHourIntervals()},
+                )
+            }
+        }
+    }
+
+    private fun getDailyWeatherData() {
+        viewModelScope.launch {
+            val getDailyWeather = getDailyWeather()
+            updateState {
+                it.copy(
+                    isLoading = false,
+                    todayTemp = getDailyWeather.data?.get(0)?.maxTemp!!.toInt(),
+                    todayMinTemp = getDailyWeather.data[0].minTemp!!.toInt(),
+                    windSpeed = getDailyWeather.data[0].dayWindSpeed!!,
+                    precipitation = getDailyWeather.data[0].precipitation!!.toInt(),
+                    days = getDailyWeather.data.map { daysForCast -> daysForCast.toDaysInterval() }
+                )
+            }
+        }
+    }
+
+    private fun searchCityWeather(city: String) {
+        viewModelScope.launch {
+            val searchCityUseCase = searchCityUseCase(city)
+
+        }
+    }
+
+}
+
+
+fun HourlyWeather.toHourIntervals(): DayHourIntervals {
+    return DayHourIntervals(
+        hour = localtime!!,
+        weatherType = weatherType!!,
+        temp = temp_c?.toInt()!!,
+    )
+}
+
+fun DaysForCast.toDaysInterval(): DaysInterval {
+    return DaysInterval(
+        date = date!!,
+        weatherType = weatherType!!,
+        maxTemp = maxTemp!!,
+        minTemp = minTemp!!,
+    )
+}
