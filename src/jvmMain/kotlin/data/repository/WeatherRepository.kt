072c48b53d16beb3f5e97d0b524361b62beb8416
YRRAAMM <yr925344git@gmail.com>
2023-07-15 16:25:20 +0300

    update repository

--- src/jvmMain/kotlin/data/repository/WeatherRepository.kt
+++ src/jvmMain/kotlin/data/repository/WeatherRepository.kt
@@ -1,16 +1,12 @@
 package data.repository
 
-import domain.model.DailyWeather
-import domain.model.HourlyWeather
 import data.remote.dto.CurrentLocation
 import data.remote.dto.WeatherForecastDto
-import domain.model.Forecast
 
 interface WeatherRepository  {
     suspend fun getCurrentLocation(): CurrentLocation
-    suspend fun getDailyWeather(lat:Double,lon:Double): List<DailyWeather>
-    suspend fun getHourWeather(lat:Double,lon:Double): List<HourlyWeather>
-    suspend fun getForecastDay() : WeatherForecastDto
 
-    suspend fun searchForecastDay(city: String): List<Forecast>
+    suspend fun getForecasts(latLongLocation: String) : WeatherForecastDto
+
+    suspend fun searchForecastDay(city: String): WeatherForecastDto
 }
