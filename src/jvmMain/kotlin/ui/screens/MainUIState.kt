cbf300dc54ad511cfe3ab22c9a7b8659f9574f8c
kareem <kareemesam52@yahoo.com>
2023-07-17 12:34:19 +0300

    updateMainUiState

--- src/jvmMain/kotlin/ui/screens/MainUIState.kt
+++ src/jvmMain/kotlin/ui/screens/MainUIState.kt
@@ -1,15 +1,19 @@
 package ui.screens
 
+import ui.theme.ImageFoggy
+
 data class MainUIState(
     val isLoading: Boolean = true,
     val isError: Boolean = false,
-    val location: String = "",
+    val location: String? = "",
     val todayTemp: Int = 0,
     val todayMinTemp: Int = 0,
     val windSpeed: Double = 0.0,
     val precipitation: Int = 0,
+    val backGround: String = ImageFoggy,
     val date: String = "",
     val weatherType: String = "",
+    val isSearching:Boolean = false,
     val hours: List<DayHourIntervals> = emptyList(),
     val days: List<DaysInterval> = emptyList(),
 )
@@ -27,5 +31,6 @@ data class DaysInterval(
     val weatherType: String = "",
     val maxTemp: Double = 0.0,
     val minTemp: Double = 0.0,
+    val backGround: String,
     val hours: List<DayHourIntervals> = emptyList(),
 )
