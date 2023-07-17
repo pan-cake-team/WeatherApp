f873f0968863a6701e2d4aefa452398d934db6aa
YRRAAMM <yr925344git@gmail.com>
2023-07-17 07:06:54 +0300

    add data to screen from api

--- src/jvmMain/kotlin/ui/screens/composable/LeftSide.kt
+++ src/jvmMain/kotlin/ui/screens/composable/LeftSide.kt
@@ -1,7 +1,6 @@
 package ui.screens.composable
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
@@ -47,7 +46,7 @@ fun LeftSide(
                 contentPadding = PaddingValues(start = 16.dp),
             ) {
                 items(state.hours) { hourlyData ->
-                    WeatherTimeCard(time = hourlyData.hour, number = hourlyData.temp)
+                    WeatherTimeCard(time = hourlyData.hour, number = hourlyData.temp, icons = hourlyData.icons)
                 }
             }
         }
