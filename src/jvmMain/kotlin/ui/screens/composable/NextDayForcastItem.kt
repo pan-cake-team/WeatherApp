214e00e7189037dfffab950e783f0c69296fef49
kareem <kareemesam52@yahoo.com>
2023-07-20 18:12:59 +0300

    fix forCastITem

--- src/jvmMain/kotlin/ui/screens/composable/NextDayForcastItem.kt
+++ src/jvmMain/kotlin/ui/screens/composable/NextDayForcastItem.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -70,6 +71,7 @@ fun NextDayForCastItem(
                 Text(text = daysInterval.date, style = typography.h5, color = TextPrimary)
                 Text(text = daysInterval.weatherType, style = typography.h6, color = TextSecondary)
             }
+            Spacer(modifier = Modifier.weight(1f))
             HorizontalDivider()
 
             Column(verticalArrangement = Arrangement.SpaceBetween, modifier = Modifier) {
